5bcc1138266bf2f8bbc514e3cddb22a62a757491
Danny Coulombe <danny.coulombe@yolarx.com>
2025-04-16 17:59:03 -0400

    new typed interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+type SizeUnit = 'B' | 'K' | 'M' | 'G' | 'T';
+
 export interface JmsSection<N> {
   name: N,
   paths: string[],
 }
 
-type SizeUnit = 'B' | 'K' | 'M' | 'G' | 'T';
 export interface JmsSettings {
   postMaxSize: `${number}${SizeUnit}`
   publicUrl: string,
@@ -14,10 +15,67 @@ export interface JmsSettings {
 
 export const defaultSettings: JmsSettings = {
   postMaxSize: '2M',
-  publicUrl: window.location.origin,
+  publicUrl: typeof window !== 'undefined' ? window.location.origin : '',
   supportedFeatures: [],
   uploadMaxSize: '2M',
   version: '1.0.0',
+}
+
+export type TRule = { regex: string, message: string }
+export type TEnum = {[key: string]: string[]} | {[key: string]: {[key: string]: string}}
+export type TSchema = {[key: string]: { [key: string]: IField }}
+
+export interface IField {
+  type: string
+  label: string
+  required?: boolean
+  default?: any
+  icon?: string
+  hint?: string
+  multiple?: boolean
+  inline?: boolean
+  min?: number
+  max?: number
+  length?: number
+  step?: number
+  'half-increments'?: boolean
+  prepend?: string
+  append?: string
+  accept?: string | string[]
+  'append-inner'?: string
+  'prepend-inner'?: string
+  fields: {[key: string]: IField}
+  items?: {[key: string]: string} | string[] | string
+  conditional?: string
+  rules?: TRule[]
+  collapsable?: boolean
+  collapsed?: boolean
+  swatches?: boolean
+  canvas?: boolean
+  inputs?: boolean
+  sliders?: boolean
+}
+
+export interface JmsStructure {
+  global: {
+    title: string,
+    icon?: string,
+    preview?: string,
+  },
+  locales: { [key: string]: string },
+  enums: { [key: string]: { [key: string]: string } },
+  schemas: { [key: string]: IField },
+  sections: { [key: string]: IField },
+}
+
+export const defaultStructure: JmsStructure = {
+  global: {
+    title: 'Untitled',
+  },
+  locales: {},
+  enums: {},
+  schemas: {},
+  sections: {}
 }
 
 export function useJsonMs() {
@@ -26,6 +84,7 @@ export function useJsonMs() {
     data: {},
     locale: null,
     settings: {},
+    structure: {},
   };
 
   const handler = {
@@ -68,54 +127,78 @@ export function useJsonMs() {
     return applyParams(getValueByPath(proxy.data, path), params);
   }
 
-  const bindToEditor = <S = string>(options: {
+  const bindToEditor = <S = string, L = string>(options: {
     targetOrigin?: string,
     onDataChange?: (data: any) => void,
-    onLocaleChange?: (locale: string) => void,
+    onLocaleInit?: (locale: L) => void,
+    onLocaleChange?: (locale: L) => void,
+    onSectionInit?: (section: JmsSection<S>) => void,
     onSectionChange?: (section: JmsSection<S>) => void,
+    onSettingsInit?: (settings: JmsSettings) => void,
+    onSettingsChange?: (settings: JmsSettings) => void,
+    onStructureInit?: (structure: JmsStructure) => void,
+    onStructureChange?: (structure: JmsStructure) => void,
   } = {}) => {
 
     options.targetOrigin = options.targetOrigin ?? '*';
 
-    window.parent.postMessage({name: 'jsonms', type: 'init'}, options.targetOrigin);
+    if (typeof window !== 'undefined') {
+      window.parent.postMessage({name: 'jsonms', type: 'init'}, options.targetOrigin);
 
-    window.addEventListener('message', (event) => {
-      if (event.data.name === 'jsonms') {
-        switch (event.data.type) {
-          case 'data':
-            const data = JSON.parse(event.data.data);
-            proxy.data = data.data;
-            proxy.settings = data.settings;
-            if (options.onDataChange) {
-              options.onDataChange(data);
-            }
-            if (options.onSectionChange) {
-              options.onSectionChange(data.section);
-            }
-            if (options.onLocaleChange) {
-              options.onLocaleChange(data.locale);
-            }
-            break;
-          case 'section':
-            if (options.onSectionChange) {
-              try {
-                options.onSectionChange(JSON.parse(event.data.data));
-              } catch (e) {
-                console.error(e);
+      window.addEventListener('message', (event) => {
+        if (event.data.name === 'jsonms') {
+          switch (event.data.type) {
+            case 'data':
+              const data = JSON.parse(event.data.data);
+              proxy.data = data.data;
+              proxy.settings = data.settings;
+              if (options.onDataChange) {
+                options.onDataChange(data);
               }
-            }
-            break;
-          case 'locale':
-            if (options.onLocaleChange) {
-              options.onLocaleChange(event.data.data);
-            }
-            break;
-          case 'reload':
-            window.location.reload();
-            break;
+              if (options.onSectionInit) {
+                options.onSectionInit(data.section);
+              }
+              if (options.onLocaleInit) {
+                options.onLocaleInit(data.locale);
+              }
+              if (options.onSettingsInit) {
+                options.onSettingsInit(data.settings);
+              }
+              if (options.onStructureInit) {
+                options.onStructureInit(data.structure);
+              }
+              break;
+            case 'section':
+              if (options.onSectionChange) {
+                try {
+                  options.onSectionChange(JSON.parse(event.data.data));
+                } catch (e) {
+                  console.error(e);
+                }
+              }
+              break;
+            case 'locale':
+              if (options.onLocaleChange) {
+                options.onLocaleChange(event.data.data);
+              }
+              break;
+            case 'settings':
+              if (options.onSettingsChange) {
+                options.onSettingsChange(event.data.data);
+              }
+              break;
+            case 'structure':
+              if (options.onStructureChange) {
+                options.onStructureChange(event.data.data);
+              }
+              break;
+            case 'reload':
+              window?.location.reload();
+              break;
+          }
         }
-      }
-    });
+      });
+    }
   }
 
   return {
